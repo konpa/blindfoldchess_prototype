46e8e2cab1b9e06bcab8b132d9f7bee3af45bf0c
konpa <julien.monty@gmail.com>
2022-01-15 17:15:23 +0100

    Add tailwind, user info and localstorage

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,25 +1,65 @@
 <template>
   <div class="login">
-    <div v-if="token">
-      <ul>
-        <li>Username: {{ username }}</li>
-        <li>Token: {{ token }}</li>
-      </ul>
-      <button v-if="token" v-on:click="logout">Logout</button>
+    <div class="bg-white shadow overflow-hidden sm:rounded-lg">
+      <div class="px-4 py-5 sm:px-6">
+        <h3 class="text-lg leading-6 font-medium text-gray-900">
+          Blindfold LiChess
+        </h3>
+        <p class="mt-1 max-w-2xl text-sm text-gray-500">
+          Blindfold chess for lichess.org
+        </p>
+      </div>
+      <div v-if="lichessAccessToken" class="border-t border-gray-200">
+        <dl>
+          <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-500">
+              Username
+            </dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              {{ user.username }}
+            </dd>
+          </div>
+          <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-500">
+              Link to profile
+            </dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              <a :href="user.url" target="_blank">{{ user.url }}</a>
+            </dd>
+          </div>
+          <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+            <dt class="text-sm font-medium text-gray-500">
+              Correspondence
+            </dt>
+            <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              {{ user.perfs.correspondence.rating }} / {{ user.perfs.correspondence.games }} game(s)
+            </dd>
+          </div>
+        </dl>
+      </div>
     </div>
-    <div v-else>
-      <button v-on:click="login">Login with lichess.org</button>
+    <div class="mt-8 space-y-6">
+      <div v-if="lichessAccessToken">
+        <t-button
+          v-if="lichessAccessToken"
+          v-on:click="logout"
+          class="w-full"
+          variant="error"
+        >Logout</t-button>
+      </div>
+      <div v-else>
+        <t-button v-on:click="login" class="w-full">Login with lichess.org</t-button>
+      </div>
+      <t-alert v-if="error" variant="danger" show>
+        {{ error }}
+      </t-alert>
     </div>
-
-    <pre v-if="error">
-      {{ error }}
-    </pre>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import { AccessContext, HttpClient, OAuth2AuthCodePKCE } from '@bity/oauth2-auth-code-pkce';
+import { AccessContext, OAuth2AuthCodePKCE } from '@bity/oauth2-auth-code-pkce';
 
 export default Vue.extend({
   data() {
@@ -42,8 +82,8 @@ export default Vue.extend({
         },
       }),
       accessContext: {} as AccessContext,
-      username: '' as string,
-      token: '' as string,
+      user: {} as Record<string, unknown>,
+      lichessAccessToken: '' as string,
       error: '' as unknown,
     };
   },
@@ -51,45 +91,53 @@ export default Vue.extend({
     login() {
       this.oauth.fetchAuthorizationCode();
     },
-    async getUsername(fetch: HttpClient) {
-      const res = await fetch('https://lichess.org/api/account');
-      this.username = (await res.json()).username;
+    async getUsername() {
+      const res = await fetch('https://lichess.org/api/account', {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${this.lichessAccessToken}`,
+        },
+      });
+      this.user = (await res.json());
     },
     async logout() {
       this.accessContext = {};
       this.error = '';
-      this.username = '';
+      this.user = {};
 
       // Example request using vanilla fetch: Revoke access token.
       await fetch('https://lichess.org/api/token', {
         method: 'DELETE',
         headers: {
-          Authorization: `Bearer ${this.token}`,
+          Authorization: `Bearer ${this.lichessAccessToken}`,
         },
       });
 
-      this.token = '';
+      this.lichessAccessToken = '';
+      localStorage.removeItem('lichessAccessToken');
     },
   },
   async mounted() {
-    try {
-      const hasAuthCode = await this.oauth.isReturningFromAuthServer();
-      if (hasAuthCode) {
-        // Might want to persist accessContext.token until the user logs out.
-        this.accessContext = await this.oauth.getAccessToken();
+    if (localStorage.lichessAccessToken) {
+      this.lichessAccessToken = localStorage.lichessAccessToken;
+      await this.getUsername();
+    } else {
+      try {
+        const hasAuthCode = await this.oauth.isReturningFromAuthServer();
+        if (hasAuthCode) {
+          // Might want to persist accessContext.token until the user logs out.
+          this.accessContext = await this.oauth.getAccessToken();
 
-        if (this.accessContext.token) {
-          this.token = this.accessContext.token.value;
+          if (this.accessContext.token) {
+            this.lichessAccessToken = this.accessContext.token.value;
+            localStorage.lichessAccessToken = this.accessContext.token.value;
+          }
+
+          await this.getUsername();
         }
-
-        // Can also use this convenience wrapper for fetch() instead of
-        // using manually using getAccessToken() and setting the
-        // "Authorization: Bearer ..." header.
-        const fetch = this.oauth.decorateFetchHTTPClient(window.fetch);
-        await this.getUsername(fetch);
+      } catch (err: unknown) {
+        this.error = err;
       }
-    } catch (err: unknown) {
-      this.error = err;
     }
   },
 });
