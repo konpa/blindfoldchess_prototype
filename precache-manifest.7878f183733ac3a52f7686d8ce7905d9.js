1eec62b0b7f91c7520f926dcbbd59ac057ac645d
konpa <julien.monty@gmail.com>
2022-01-17 15:41:06 +0100

    Build dist

---
+++ precache-manifest.7878f183733ac3a52f7686d8ce7905d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb2c0d41166d4147989d",
+    "url": "/blindfoldchess/css/app.f659c3fc.css"
+  },
+  {
+    "revision": "4b3d53b119ab79053ac4072ab5cd486e",
+    "url": "/blindfoldchess/index.html"
+  },
+  {
+    "revision": "eb2c0d41166d4147989d",
+    "url": "/blindfoldchess/js/app.c5cf156b.js"
+  },
+  {
+    "revision": "eb54b1e5b61f1f0937cf",
+    "url": "/blindfoldchess/js/chunk-vendors.0992e65c.js"
+  },
+  {
+    "revision": "4de3673fd544acb10b8f059434387ffd",
+    "url": "/blindfoldchess/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/blindfoldchess/robots.txt"
+  }
+]);
